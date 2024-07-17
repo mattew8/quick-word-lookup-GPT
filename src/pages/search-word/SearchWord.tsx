0a396bd562789cf7f708b8cbc468229c6ae33de0
mattew8 <mattew4483@likelion.org>
2024-07-17 23:30:20 +0900

    ✅ search word input

--- src/pages/search-word/SearchWord.tsx
+++ src/pages/search-word/SearchWord.tsx
@@ -1,8 +1,31 @@
+import { generateText } from '@/shared/translate/translator';
+import { FormEventHandler, useState } from 'react';
+
 const SearchWord = () => {
+  const [answer, setAnswer] = useState<string>('');
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const fieldValue = Object.fromEntries(formData.entries())['search-input'];
+
+    try {
+      const message = await generateText(fieldValue.toString());
+      if (message === null) return;
+      setAnswer(message);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div>
       <h1>단어를 검색해보세요</h1>
-      <input placeholder="search words!" />
+      <form onSubmit={onSubmit}>
+        <input placeholder="search words!" name="search-input" />
+        <button>검색</button>
+      </form>
+      {answer && <p>{answer}</p>}
     </div>
   );
 };
