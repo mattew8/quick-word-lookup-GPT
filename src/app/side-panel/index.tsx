5b1ab15ba2bbc3a10b1c3d837f5dd96cac5f02cc
mattew8 <mattew4483@likelion.org>
2024-07-20 23:47:23 +0900

    📦 radix-ui

--- src/app/side-panel/index.tsx
+++ src/app/side-panel/index.tsx
@@ -1,12 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import SearchWord from '@/pages/search-word/SearchWord';
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
 
 async function initialize() {
   const rootElement = document.getElementById('root');
   if (rootElement === null) return;
 
   const root = ReactDOM.createRoot(rootElement);
-  root.render(<SearchWord />);
+  root.render(
+    <Theme>
+      <SearchWord />
+    </Theme>,
+  );
 }
 
 initialize();
