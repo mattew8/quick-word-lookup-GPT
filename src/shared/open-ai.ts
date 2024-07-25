3c27a391ada46cd8212a5f364680b5ae3a258975
mattew8 <mattew4483@likelion.org>
2024-07-25 23:07:28 +0900

    ✨ open ai and translation model

---
+++ src/shared/open-ai.ts
@@ -0,0 +1,31 @@
+import OpenAI from 'openai';
+import {
+  ChatCompletion,
+  ChatCompletionMessageParam,
+} from 'openai/src/resources/index.js';
+
+export interface OpenAIClient {
+  chatCompletionsCreate(params: {
+    messages: ChatCompletionMessageParam[];
+    model: string;
+  }): Promise<ChatCompletion>;
+}
+
+// 실제 OpenAI 클라이언트 구현체
+export class OpenAIClientImpl implements OpenAIClient {
+  private openai: OpenAI;
+
+  constructor(apiKey: string) {
+    this.openai = new OpenAI({
+      apiKey: apiKey,
+      dangerouslyAllowBrowser: true,
+    });
+  }
+
+  async chatCompletionsCreate(params: {
+    messages: ChatCompletionMessageParam[];
+    model: string;
+  }): Promise<ChatCompletion> {
+    return this.openai.chat.completions.create(params);
+  }
+}
