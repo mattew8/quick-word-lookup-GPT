4bfdc4566e78d4bd11aa4cb7deb5f52eae065f42
mattew8 <mattew4483@likelion.org>
2024-07-26 13:55:24 +0900

    ✅ shared ui

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,17 @@
+// common components
+export {
+  Button,
+  Flex,
+  Spinner,
+  TextField,
+  IconButton,
+  Text,
+  Blockquote,
+} from '@radix-ui/themes';
+
+// icons
+export {
+  MagnifyingGlassIcon,
+  CheckIcon,
+  CopyIcon,
+} from '@radix-ui/react-icons';
